c20eacd2c176c8ea6f55ddf07a9bf9d1b566fd01
AKASHDEEP-ROY <57352303+AKASHDEEP-ROY@users.noreply.github.com>
2019-11-06 19:05:42 +0530

    Update power.py

--- power.py
+++ power.py
@@ -7,13 +7,12 @@ About : finding value of a parameter that maps or do not map to a list
 L=[1,2,4,8,16,32,64]
 X=5
 
-# by while else construction :
-k=0
-while k<len(L):
-   if (2**X==L[k]):
-       print("element required is at index =",k)
-       break
-   else:
-       k=k+1
+# for-else construction & list index method
+
+for n in L :
+    if 2**X==n:
+        print('element required is at index =',L.index(n))
+        break
 else:
-    print(X,"not found")
+    print(X,'not found')
+
