469d828da23acbe4857fa1662b20efe3d49bc087
AKASHDEEP-ROY <57352303+AKASHDEEP-ROY@users.noreply.github.com>
2019-11-06 19:10:05 +0530

    Update power.py

--- power.py
+++ power.py
@@ -7,9 +7,11 @@ About : finding value of a parameter that maps or do not map to a list
 L=[1,2,4,8,16,32,64]
 X=5
 
-# in operator memebership expression
-if(2**X in L):
-    print('element required is at index=',L.index(2**X))
-else:
-    print(X,'not found')
+#for loop & list append method
+List=[]
+b=int(input("the max power of 2 in the list ="))
+for q in range(0,b+1):
+    List.append(2**q)
+print("List of the power-of-2 list =",List)
+
 
